e341c592820ab4bc38120c87749f19c547b2f00b
SalsabillaMA <salsabilla_maurettasya_28rpl@student.smktelkom-mlg.sch.id>
2021-11-05 17:19:58 +0700

    splashscreen layout
    update theme noactionbar

---
+++ app/src/main/java/com/projectmoja/mokletpay/ui/splashscreen/SplashScreenActivity.java
@@ -0,0 +1,16 @@
+package com.projectmoja.mokletpay.ui.splashscreen;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.projectmoja.mokletpay.R;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+    }
+}
