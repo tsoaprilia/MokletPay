b1f2d6f946e36dd22a73cd017ec0d8494452a39c
SalsabillaMA <salsabilla_maurettasya_28rpl@student.smktelkom-mlg.sch.id>
2021-11-11 08:12:58 +0700

    layouting home siswa

---
+++ app/src/main/java/com/projectmoja/mokletpay/ui/home/homesiswa/HomeSiswaActivity.java
@@ -0,0 +1,16 @@
+package com.projectmoja.mokletpay.ui.home.homesiswa;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.projectmoja.mokletpay.R;
+
+public class HomeSiswaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_siswa);
+    }
+}
