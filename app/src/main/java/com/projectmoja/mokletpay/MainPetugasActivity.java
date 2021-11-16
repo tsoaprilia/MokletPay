476795cba7c5a62df687801325d8a5b149b983fa
tsoaprilia <tsoaprilia@gmail.com>
2021-11-16 12:22:02 +0700

    Layout Main Activity Petugas

---
+++ app/src/main/java/com/projectmoja/mokletpay/MainPetugasActivity.java
@@ -0,0 +1,14 @@
+package com.projectmoja.mokletpay;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainPetugasActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_petugas2);
+    }
+}
