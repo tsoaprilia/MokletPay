2d462f691646518d4295d4eb1b904d0fc2307fae
tsoaprilia <tsoaprilia@gmail.com>
2021-11-16 12:31:55 +0700

    Update Kurang Bayar Activity

---
+++ app/src/main/java/com/projectmoja/mokletpay/KurangBayarActivity.java
@@ -0,0 +1,14 @@
+package com.projectmoja.mokletpay;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class KurangBayarActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_kurang_bayar2);
+    }
+}
